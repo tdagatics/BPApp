691f25193bbad53fbb4062c5412bc9cdb4f76324
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-28 13:58:24 -0700

    Initial Commit

---
+++ BPApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BPApp
+//
+//  Created by Anthony Dagati on 10/28/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
